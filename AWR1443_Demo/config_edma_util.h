66367a9453d94f2bc870b6bc43176792cc2eb235
juruoWYC <wangyucong_one@126.com>
2018-02-28 16:41:47 +0800

    1st_revise_by_WYC

--- AWR1443_Demo/config_edma_util.h
+++ AWR1443_Demo/config_edma_util.h
@@ -1,32 +1,26 @@
 #ifndef _CONFIGEDMA_H
 #define _CONFIGEDMA_H
 
-#include <ti/drivers/edma/edma.h>
+#include <ti/drivers/edma/edma.h>  //EDMA底层驱动程序
 
 /**
- *  @b Description
- *  @n
- *     Utility function for configuring a simple EDMA transfer used for transferring
- *     a specific  32-bit word (from a table of 16 words each containing a 1-hot signature) 
- *     to the HWA's DMA completion register. The counts and addresses
- *     can be obtained from HWA APIs issued for configuring HWA. 
- *     This is used to implement the DMA based trigger mode 
- *     of the HWACC (search for "DMA based trigger" in the HWACC spec document)
+ * 用于配置简单EDMA传输的API函数，可将特定的32位字(从各包含1个One-Hot签名的16字表中)传输
+ * 到HWA的DMA完成寄存器。计数器和地址可以从用于配置HWA的API函数中获得。该函数用于实现基于
+ * DMA的HWACC触发模式
  *
- *  @param[in]  handle  EDMA handle.
- *  @param[in]  chId    Channel ID
- *  @param[in]  isEventTriggered true if chId is event triggered else false
- *  @param[in]  pSrcAddress Pointer to source address.
- *  @param[in]  pDestAddress Pointer to destination address.
- *  @param[in]  aCount A count.
- *  @param[in]  bCount B count. 
- *  @param[in]  cCount C count.
- *  @param[in]  linkChId Link Channel Id (or rather PaRAM Id) for shadow linking 
- *             (reloading to prevent reprogramming).
+ * 参数:
+ * handle            指向EDMA通道的句柄
+ * chId              Channel ID
+ * isEventTriggered  标记Channel ID是否由事件触发
+ * pSrcAddress       源地址指针
+ * pDestAddress      目标地址指针
+ * aCount            A计数器
+ * bCount            B计数器
+ * cCount            C计数器
+ * linkChId Link     用于链接的Channel ID,相当于PaRAM ID(重新装载以防止重编程)
  *
- *  @retval
- *      EDMA driver error code, see "EDMA_ERROR_CODES" in EDMA API.
- */
+ * 返回值:错误代码,定义详见edma.h
+ **/
 int32_t EDMAutil_configHwaOneHotSignature(EDMA_Handle handle, 
     uint8_t chId,
     bool isEventTriggered,
@@ -38,33 +32,26 @@ int32_t EDMAutil_configHwaOneHotSignature(EDMA_Handle handle,
     uint16_t linkChId);
 
 /**
- *  @b Description
- *  @n
- *     Utility function for configuring EDMA for a transpose transfer 
- *    (used to write 1D-FFT output to L3 in a transpose fashion).
+ * 将EDMA配置为转置传输的API函数(用于以转置的方式将1D-FFT输出写入L3缓存)
  *
- *  @param[in]  handle      EDMA handle.
- *  @param[in]  chId        Channel ID
- *  @param[in]  linkChId    Link Channel Id (or rather PaRAM Id) 
- *                          for shadow linking (reloading to prevent reprogramming).
- *  @param[in]  chainChId   Chain Channel Id, chId will be chained to this.
- *  @param[in]  pSrcAddress Pointer to source address.
- *  @param[in]  pDestAddress Pointer to destination address.
- *  @param[in]  numAnt      Number of receive antennae.
- *  @param[in]  numRangeBins Number of range bins.
- *  @param[in]  numChirpsPerFrame Number of chirps per frame.
- *  @param[in]  isIntermediateChainingEnabled Set to 'true' if intermediate transfer chaining is to be
- *                 enabled.
- *  @param[in]  isFinalChainingEnabled Set to 'true' if final transfer chaining to be
- *                 enabled.
- *  @param[in]  isTransferCompletionEnabled Set to 'true' if final transfer completion 
- *                 indication is to be enabled.
- *  @param[in]  transferCompletionCallbackFxn Transfer completion call back function.
- *  @param[in]  transferCompletionCallbackFxnArg Argument for transfer completion call back function.
+ * 参数:
+ * handle                            指向EDMA通道的句柄
+ * chId                              Channel ID
+ * linkChId Link                     用于链接的Channel ID,相当于PaRAM ID(重新装载以防止重编程)
+ * chainChId                         Chain Channel Id, chId will be chained to this.
+ * pSrcAddress                       源地址指针
+ * pDestAddress                      目标地址指针
+ * numAnt                            接收天线编号
+ * numRangeBins                      1D-FFT点数
+ * numChirpsPerFrame                 每帧的啁啾数
+ * isIntermediateChainingEnabled     Set to 'true' if intermediate transfer chaining is to be enabled.
+ * isFinalChainingEnabled            Set to 'true' if final transfer chaining to be enabled.
+ * isTransferCompletionEnabled       Set to 'true' if final transfer completion indication is to be enabled.
+ * transferCompletionCallbackFxn     数据传输完毕的回调函数
+ * transferCompletionCallbackFxnArg  数据传输完毕的回调函数实参(详见edma.h)
  *
- *  @retval
- *      EDMA driver error code, see "EDMA_ERROR_CODES" in EDMA API.
- */    
+ * 返回值:错误代码,定义详见edma.h
+ */
 int32_t EDMAutil_configHwaTranspose(EDMA_Handle handle,
     uint8_t   chId,
     uint16_t  linkChId,
@@ -81,38 +68,27 @@ int32_t EDMAutil_configHwaTranspose(EDMA_Handle handle,
 	uintptr_t transferCompletionCallbackFxnArg);
 
 /**
- *  @b Description
- *  @n
- *     Utility function for configuring EDMA for a contiguous (as opposed to transpose)
- *    transfer.
+ * 将EDMA配置为连续传输的API函数
  *
- *  @param[in]  handle      EDMA handle.
- *  @param[in]  chId        Channel ID
- *  @param[in]  isEventTriggered true if chId is event triggered else false.
- *  @param[in]  linkChId    Link Channel Id (or rather PaRAM Id) 
- *                          for shadow linking (reloading to prevent reprogramming).
- *  @param[in]  chainChId   Chain Channel Id, chId will be chained to this.
- *  @param[in]  pSrcAddress Pointer to source address.
- *  @param[in]  pDestAddress Pointer to destination address.
- *  @param[in]  numBytes    Number of bytes, determines aCount (the 1st dimension of transfer).
- *  @param[in]  numBlocks   Number of blocks, determines bCount (the 2nd dimension of transfer).
- *  @param[in]  srcIncrBytes Source increment bytes, determines sourceBindex 
- *                           (jump in source address in bytes after every 1st 
- *                            dimension (aCount related) transfer).
- *  @param[in]  dstIncrBytes Destination increment bytes, determines destinatinoBindex
- *                           (jump in destination address in bytes after every 1st 
- *                            dimension (aCount related) transfer).
- *  @param[in]  isIntermediateChainingEnabled Set to 'true' if intermediate transfer chaining is to be
- *                 enabled.
- *  @param[in]  isFinalChainingEnabled Set to 'true' if final transfer chaining to be
- *                 enabled.
- *  @param[in]  isTransferCompletionEnabled Set to 'true' if final transfer completion 
- *                 indication is to be enabled.
- *  @param[in]  transferCompletionCallbackFxn Transfer completion call back function.
- *  @param[in]  transferCompletionCallbackFxnArg Argument for transfer completion call back function.
+ * 参数:
+ * handle                            指向EDMA通道的句柄
+ * chId                              Channel ID
+ * isEventTriggered                  标记Channel ID是否由事件触发
+ * linkChId Link                     用于链接的Channel ID,相当于PaRAM ID(重新装载以防止重编程)
+ * chainChId                         Chain Channel Id, chId will be chained to this.
+ * pSrcAddress                       源地址指针
+ * pDestAddress                      目标地址指针
+ * numBytes                          字节数,决定aCount(传输器的第一个维度)
+ * numBlocks                         数据块数,决定bCount(传输器的第二个维度)
+ * srcIncrBytes                      源地址的增量字节数,决定sourceBindex(每次一维传输后源地址跳跃的字节数)
+ * dstIncrBytes                      目标地址的增量字节数,决定destinatinoBindex(每次一维传输后目标地址跳跃的字节数)
+ * isIntermediateChainingEnabled     Set to 'true' if intermediate transfer chaining is to be enabled.
+ * isFinalChainingEnabled            Set to 'true' if final transfer chaining to be enabled.
+ * isTransferCompletionEnabled       Set to 'true' if final transfer completion indication is to be enabled.
+ * transferCompletionCallbackFxn     数据传输完毕的回调函数
+ * transferCompletionCallbackFxnArg  数据传输完毕的回调函数实参(详见edma.h)
  *
- *  @retval
- *      EDMA driver error code, see "EDMA_ERROR_CODES" in EDMA API.
+ * 返回值:错误代码,定义详见edma.h
  */
 int32_t EDMAutil_configHwaContiguous(EDMA_Handle handle,
     uint8_t   chId,
